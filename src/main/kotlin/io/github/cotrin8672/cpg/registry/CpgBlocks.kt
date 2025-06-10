9b5f767d90a70a6e82abe85b11c962c1a4e85072
cotrin1208 <gummy8672@gmail.com>
2025-06-10 09:25:04 +0900

    added colored pure glass

--- src/main/kotlin/io/github/cotrin8672/cpg/registry/CpgBlocks.kt
+++ src/main/kotlin/io/github/cotrin8672/cpg/registry/CpgBlocks.kt
@@ -1,23 +1,32 @@
 package io.github.cotrin8672.cpg.registry
 
+import com.simibubi.create.content.decoration.palettes.ConnectedGlassBlock
 import com.simibubi.create.foundation.block.connected.SimpleCTBehaviour
 import com.simibubi.create.foundation.data.CreateRegistrate.connectedTextures
-import com.tterrag.registrate.util.entry.BlockEntry
 import io.github.cotrin8672.cpg.CreatePureGlass.REGISTRATE
-import io.github.cotrin8672.cpg.block.PureGlassBlock
+import io.github.cotrin8672.cpg.CreatePureGlass.getColors
 import net.minecraft.client.renderer.RenderType
 import net.minecraft.world.level.block.Blocks
 import java.util.function.Supplier
 
 object CpgBlocks {
-    val PURE_GLASS: BlockEntry<PureGlassBlock> = REGISTRATE
-        .block<PureGlassBlock>("pure_glass", ::PureGlassBlock)
-        .onRegister(connectedTextures { SimpleCTBehaviour(CpgSpriteShifts.PURE_GLASS) })
-        .addLayer { Supplier(RenderType::translucent) }
-        .initialProperties { Blocks.GLASS }
-        .properties { it.noOcclusion() }
-        .simpleItem()
-        .register()
+    fun registerGlass() {
+        getColors().onEach { color ->
+            REGISTRATE
+                .block(
+                    if (color.isNotBlank()) "${color}_stained_pure_glass" else "pure_glass",
+                    ::ConnectedGlassBlock
+                )
+                .onRegister(connectedTextures { SimpleCTBehaviour(CpgSpriteShifts.MAP[color]) })
+                .addLayer { Supplier(RenderType::translucent) }
+                .initialProperties { Blocks.GLASS }
+                .properties { it.noOcclusion() }
+                .simpleItem()
+                .register()
+        }
+    }
 
-    fun register() {}
+    fun register() {
+        registerGlass()
+    }
 }
--- src/main/kotlin/io/github/cotrin8672/cpg/registry/CpgSpriteShifts.kt
+++ src/main/kotlin/io/github/cotrin8672/cpg/registry/CpgSpriteShifts.kt
@@ -5,9 +5,15 @@ import com.simibubi.create.foundation.block.connected.CTSpriteShiftEntry
 import com.simibubi.create.foundation.block.connected.CTSpriteShifter
 import com.simibubi.create.foundation.block.connected.CTType
 import io.github.cotrin8672.cpg.CreatePureGlass.asResource
+import io.github.cotrin8672.cpg.CreatePureGlass.getColors
 
 object CpgSpriteShifts {
-    val PURE_GLASS = omni("pure_glass", "pure_glass")
+    val MAP = getColors().associateWith {
+        if (it.isNotBlank())
+            omni("${it}_stained_pure_glass", "${it}_stained_pure_glass")
+        else
+            omni("pure_glass", "pure_glass")
+    }
 
     private fun omni(blockTextureName: String, connectedTextureName: String) =
         getCT(AllCTTypes.OMNIDIRECTIONAL, blockTextureName, connectedTextureName)
